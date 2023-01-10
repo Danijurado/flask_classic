3649bee5a304ca2cf5679867a7f1eb53b8859b07
Danijurado <danieljp.85@gmail.com>
2023-01-10 23:10:00 +0100

    app dia 10-01-2023

--- registro_ig/routes.py
+++ registro_ig/routes.py
@@ -1,6 +1,13 @@
 from registro_ig import app
+from flask import render_template
 
 
 @app.route('/')
 def index():
-    return 'esto funciona, flask mola'
+    datos_mov=[
+        {"id":1,"date":"2022-01-01","concept":"Sueldo","quantity":1500},
+        {"id":2,"date":"2022-01-05","concept":"Regalo Reyes","quantity":-150},
+        {"id":3,"date":"2022-01-06","concept":"Almuerzo Reyes","quantity":-100},
+    ]
+    
+    return render_template('index.html',pageTitle="Todos",data=datos_mov)
